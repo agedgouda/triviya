aaceda2fafe37476180d207f0b78314f2fe5faa0
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2024-11-27 16:24:26 -0800

    location seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,9 @@ class DatabaseSeeder extends Seeder
         //User::factory(100)->withPersonalTeam()->create();
 
        $this->call([
-            //ModeSeeder::class,
+            ModeSeeder::class,
             //QuestionSeeder::class,
-            //ModeQuestionSeeder::class,
+            ModeQuestionSeeder::class,
             GameSeeder::class,
         ]);
 
