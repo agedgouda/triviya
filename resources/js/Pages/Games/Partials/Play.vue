6805825c7167198a599e7b4768eddafa81e39a70
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2025-03-16 13:32:02 -0700

    clean up answer pages

--- resources/js/Pages/Games/Partials/Play.vue
+++ resources/js/Pages/Games/Partials/Play.vue
@@ -12,7 +12,7 @@ const props = defineProps({
     questions: Object,
 });
 
-const page = ref("interstitial")
+const page = ref("landing")
 const questionNumber = ref(0)
 const timer = ref(30)
 let intervalId = null
@@ -39,35 +39,39 @@ const startCountdown = (onComplete) => {
   }, 100)
 }
 
+//startCountdown()
 const newQuestion = () => {
     page.value = "question";
     questionNumber.value += 1
-    startCountdown(() => {
-        page.value = "interstitial";
-    })
 }
 
 </script>
 <template>
-
-<div v-for="(question, index) in questions">
-   {{ index+1 }}. {{ question }} {{ question.player_name }}:  {{ question.question_text }}
-</div>
-<div>Round {{ Math.floor(questionNumber/10)+1  }}</div>
-    <div v-if="page  === 'interstitial'">
-        <div v-if="questionNumber  === 0">
+    <div v-if="questionNumber  === 0">
+        <div >
             Landing Page Text
         </div>
-        <div v-else>
-            Text before {{ questionNumber+1  }}
-        </div>
-        <PrimaryButton @click="newQuestion()" class="my-4">
-            &nbsp;Go to question {{ questionNumber+1  }}
+        <PrimaryButton @click="questionNumber = 1" class="my-4">
+            &nbsp;Go to first question
         </PrimaryButton>
     </div>
-    <div v-if="page  === 'question'">
-        Who answered "<span>{{questions[questionNumber].question_text}}</span>""
-        <div>{{questions[questionNumber].answer}}</div>
+    <div v-else>
+        <div>Round {{ Math.floor(questionNumber/10)+1  }}</div>
+        <div>Question {{ questions[questionNumber-1].question_number%10  }}</div>
+        Who answered "<span>{{questions[questionNumber-1].question_text}}</span>""
+        <div>{{questions[questionNumber-1].answer}}</div>
+        <PrimaryButton @click="questionNumber -= 1" class="my-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-2 h-2 rotate-180">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+            </svg>
+            Previous
+        </PrimaryButton>
+        <PrimaryButton @click="questionNumber += 1" class="ml-2 my-2">
+            Next
+            <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="ml-2 h-2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+            </svg>
+        </PrimaryButton>
 
         {{  timer/10 }}
 
