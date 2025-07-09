b6bce5c6f38fd9c0c35b86c1f37fb9bf55b06d86
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2025-07-09 16:47:08 -0700

    questions for admin

---
+++ app/Filament/Resources/QuestionResource.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\QuestionResource\Pages;
+use App\Filament\Resources\QuestionResource\RelationManagers;
+use App\Models\Question;
+use App\Models\Mode;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\CheckboxList;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Columns\TextColumn;
+
+
+
+class QuestionResource extends Resource
+{
+    protected static ?string $model = Question::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-question-mark-circle';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('question_text')->required()->maxLength(255)->label('Question'),
+                CheckboxList::make('modes')
+                ->label('Modes')
+                ->relationship('modes', 'name')
+                ->columns(3)
+                ->default(function () {
+                    return Mode::where('name', 'Family Ties')->pluck('id')->toArray();
+                }),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('question_text')
+                    ->label('Question'),
+                TextColumn::make('modes_list')
+                    ->label('Modes')
+                    ->getStateUsing(function ($record) {
+                        return $record->modes->pluck('name')->join(', ');
+                }),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->with('modes');
+    }
+
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListQuestions::route('/'),
+            'create' => Pages\CreateQuestion::route('/create'),
+            'edit' => Pages\EditQuestion::route('/{record}/edit'),
+        ];
+    }
+}
