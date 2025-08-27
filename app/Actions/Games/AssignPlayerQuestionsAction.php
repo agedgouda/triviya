672427e9bb4726b8f2c10d12ba45527486107f7f
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2025-08-27 11:11:41 -0700

    make questions unique

--- app/Actions/Games/AssignPlayerQuestionsAction.php
+++ app/Actions/Games/AssignPlayerQuestionsAction.php
@@ -7,14 +7,26 @@ use App\Models\User;
 use App\Models\GameUser;
 use App\Models\GameUserQuestions;
 
+use Illuminate\Support\Facades\DB;
+
 class AssignPlayerQuestionsAction
 {
 
     public function handle(Game $game, User $user)
     {
 
-        // Select 10 random questions from the game
-        $selectedQuestions = $game->questions->shuffle()->take(10);
+        $assignedQuestions = GameUserQuestions::where('game_id', $game->id)
+            ->pluck('question_text')
+            ->toArray();
+
+
+        // Select 10 random questions from the game that haven't been used yet
+        $selectedQuestions = $game->questions()
+            ->whereNotIn('question_text', $assignedQuestions)
+            ->inRandomOrder()
+            ->limit(10)
+            ->get();
+
 
         // Prepare bulk insert data for question assignments
         $assignments = [];
