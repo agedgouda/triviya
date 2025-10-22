e509028be8e0f1bd20406187fc7f06c5756cefec
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2025-10-22 11:06:51 -0700

    fix a bug

--- app/Actions/Games/FetchGames.php
+++ app/Actions/Games/FetchGames.php
@@ -22,23 +22,22 @@ class FetchGames
             'game_user.status as current_user_status',
             'game_user.is_host as is_host',
             DB::raw('(SELECT COUNT(*) FROM game_user WHERE game_user.game_id = games.id) as players_count'),
-        ])
-        ->paginate(10);
-
-        $query = Game::query()
-        ->join('game_user', 'games.id', '=', 'game_user.game_id')
-        ->where('game_user.user_id', $userId)
-        ->select([
-            'games.id',
-            'games.name',
-            'games.status',
-            'game_user.status as current_user_status',
-            'game_user.is_host as is_host',
-            DB::raw('(SELECT COUNT(*) FROM game_user WHERE game_user.game_id = games.id) as players_count'),
         ]);
 
-        $sql = $query->toSql();
-        \Log::info('Game query: ' . $sql, $query->getBindings());
+        // $query = Game::query()
+        // ->join('game_user', 'games.id', '=', 'game_user.game_id')
+        // ->where('game_user.user_id', $userId)
+        // ->select([
+        //     'games.id',
+        //     'games.name',
+        //     'games.status',
+        //     'game_user.status as current_user_status',
+        //     'game_user.is_host as is_host',
+        //     DB::raw('(SELECT COUNT(*) FROM game_user WHERE game_user.game_id = games.id) as players_count'),
+        // ]);
+
+        //$sql = $query->toSql();
+        //\Log::info('Game query: ' . $sql, $query->getBindings());
 
         return $games;
     }
