f11cdeeaa61319fc06a5b210135cc4cfe3a7fcc0
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2025-11-14 11:14:32 -0800

    fixing table for mobile

--- resources/js/Components/Table.vue
+++ resources/js/Components/Table.vue
@@ -1,15 +1,16 @@
 <template>
-
-        <table class="min-w-full table-auto border-collapse text-xs sm:text-sm md:text-base">
-            <thead class="bg-triviyaRegular text-white border border-triviyaRegular">
-                <tr>
-                    <slot name="header"></slot>
-                </tr>
-        </thead>
-        <tbody>
-            <slot :rowClass="rowClass"></slot> <!-- Pass rowClass as a slot prop -->
-        </tbody>
-    </table>
+  <table class="min-w-full max-w-full table-fixed border-collapse text-xs sm:text-sm md:text-base break-words">
+    <thead class="bg-triviyaRegular text-white border border-triviyaRegular">
+      <tr>
+        <!-- Slot for headers. Recommended to set widths per column -->
+        <slot name="header"></slot>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- Slot for rows. rowClass prop passed -->
+      <slot :rowClass="rowClass"></slot>
+    </tbody>
+  </table>
 </template>
 
 <script setup>
@@ -27,9 +28,9 @@ const props = defineProps({
 });
 
 const rowClass = computed(() => [
-    'border-b-8 3 border-triviya-lightBackground bg-white',
-    props.hasHover || props.hasPointer
-        ? 'cursor-pointer hover:bg-triviya-redHover hover:text-white'
-        : '',
+  'border-b-4 sm:border-b-8 border-triviya-lightBackground bg-white', // thinner border on mobile
+  'px-2 sm:px-4 py-1 sm:py-2', // add padding to table rows if not set on td
+  (props.hasHover || props.hasPointer) ? 'cursor-pointer hover:bg-triviya-redHover hover:text-white' : ''
 ].join(' '));
+
 </script>
